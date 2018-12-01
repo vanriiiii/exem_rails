cd820147b50f5c90e667e02b435f6a481bcdf44f
vanri <vanri0415@gmail.com>
2018-12-01 16:51:25 +0900

    Changes cord Schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -15,11 +15,11 @@ ActiveRecord::Schema.define(version: 20181201070843) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  # create_table "contacts", force: :cascade do |t|
-    # t.string "name"
-    # t.text "email"
-    # t.text "content"
-  # end
+  create_table "contacts", force: :cascade do |t|
+    t.string "name"
+    t.text "email"
+    t.text "content"
+  end
 
   create_table "tweets", force: :cascade do |t|
     t.text "content"
