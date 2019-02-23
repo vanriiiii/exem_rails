2dbf687930ef5d5974e536e1bbdb6ea42a2cce84
vanri <vanri0415@gmail.com>
2019-02-23 20:21:32 +0900

    model add code

--- app/models/owatter.rb
+++ app/models/owatter.rb
@@ -4,4 +4,5 @@ class Owatter < ApplicationRecord
   belongs_to :user
   has_many :favorites, dependent: :destroy
   has_many :favorite_users, through: :favorites, source: :user
+  mount_uploader :image, ImageUploader
 end
